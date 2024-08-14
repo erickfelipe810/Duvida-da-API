235d3680727bf358db8468108e86d2952a485f46
erickfelipe810 <erickfelipe810@gmail.com>
2024-08-14 17:13:11 -0300

    Duvida na Api Db games nao funciona

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { RestritoComponent } from './restrito/restrito.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"inicio" , component: InicioComponent},
+  {path:"Login" , component: LoginComponent},
+  {path:"restrito" ,component: RestritoComponent},
+  {path:"" , redirectTo:"/inicio" ,pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
