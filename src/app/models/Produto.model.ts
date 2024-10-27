10ccd470308175b8bc64b2f64244557627052284
erickfelipe810 <erickfelipe810@gmail.com>
2024-10-27 00:07:26 -0300

    Framework Front End com api

--- src/app/models/Produto.model.ts
+++ src/app/models/Produto.model.ts
@@ -1,5 +1,5 @@
 export class Produto{
-    id: number = 0
+    id: number = 0 ;
     produto: string = "";
     descricao : string = "";
     foto : string = "";
--- src/app/produto.service.ts
+++ src/app/produto.service.ts
@@ -8,30 +8,30 @@ import { Produto } from './models/Produto.model';
 })
 export class ProdutoService {
 
-  private url = "http//localhost:3000/produtos";
+  private url = "http://localhost:3000/produtos";
 
   constructor(private _httpClient: HttpClient) { }
 
   getProduto(id: any): Observable<Produto> {
-    const urlIdProduto = `${this.url}?id=${id}`;
-    return this._httpClient.get<Produto>(urlIdProduto);
+    const urlAtualizar = `${this.url}?id=${id}`;
+    return this._httpClient.get<Produto>(urlAtualizar);
   }
 
   getProdutos(): Observable<Produto[]> {
     return this._httpClient.get<Produto[]>(this.url);
   }
 
-  cadastrarProdutos(produto: Produto): Observable<Produto[]> {
+  cadastrarProduto(produto: Produto): Observable<Produto[]> {
     return this._httpClient.post<Produto[]>(this.url, produto);
   }
 
-  atualizaProduto(id: any, produto: Produto): Observable<Produto[]> {
-    const urlAtualizar = `${this.url}/?${id}`;
+  atualizarProduto(id: any, produto: Produto): Observable<Produto[]> {
+    const urlAtualizar = `${this.url}/${id}`;
     return this._httpClient.put<Produto[]>(urlAtualizar, produto);
   }
 
   removerProduto(id: any): Observable<Produto[]> {
-    const urlDeletar = `${this.url}/?${id}`;
+    const urlDeletar = `${this.url}/${id}`;
     return this._httpClient.delete<Produto[]>(urlDeletar);
   }
 }
